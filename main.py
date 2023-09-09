a164c7151e279b5ed3686a479dedb749fbcd550b
Panitapu <110778061+P-Jashu@users.noreply.github.com>
2023-09-09 17:21:57 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+from replit import clear
+from art import logo
+print(logo)
+print("\nWelcome to my auction")
+bidding = False
+bid = {}
+
+def winner(bid):
+  max_bid = 0
+  winner_name = ""
+  for i in bid:
+    if bid[i] > max_bid:
+      max_bid = bid[i]
+      winner_name = i
+  print(f"\nThe highest bidder is {winner_name} with amount of ${max_bid}")
+
+while not bidding:
+  name = input("\nEnter your name: ")
+  price = int(input("\nEnter your amount: $"))
+  bid[name] = price
+  next_bid = input("\nAny one else is there to bid? (yes or no)")
+  if next_bid == "no":
+    winner(bid)
+    bidding = True
+  else:
+    clear()
+    print(logo)
